f132af05512c2e5d61afaf577371823f17db7364
Neo87z <shehanhoradagoda12@gmail.com>
2022-11-15 16:01:28 -0330

    Update GroupChatModal.js

--- src/components/miscellaneous/GroupChatModal.js
+++ src/components/miscellaneous/GroupChatModal.js
@@ -58,7 +58,7 @@ const GroupChatModal = ({ children }) => {
           Authorization: `Bearer ${user.token}`,
         },
       };
-      const { data } = await axios.get(`/api/user?search=${search}`, config);
+      const { data } = await axios.get(`https://a38ce8477337.in.ngrok.io/api/user?search=${search}`, config);
       console.log(data);
       setLoading(false);
       setSearchResult(data);
