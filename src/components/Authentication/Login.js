6af181e4d359468afed164ee8f0d8e14a43ef654
Neo87z <shehanhoradagoda12@gmail.com>
2022-11-15 15:49:40 -0330

    Update Login.js

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -40,7 +40,7 @@ const Login = () => {
       };
 
       const { data } = await axios.post(
-        "/api/user/login",
+        "https://a38ce8477337.in.ngrok.io/api/user/login",
         { email, password },
         config
       );
